290cae921b0c216a2942e725c1a4e51bd82d15a2
aguilarcarboni <aguilarcarboni@gmail.com>
2024-04-04 10:16:36 -0600

    Update api.py

--- api.py
+++ api.py
@@ -4,27 +4,29 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 #Main
-ticker = 'AAPL'
+tickers = ['AAPL', 'SPY']
 
-tickerData = yf.Ticker(ticker)
+data = {}
 
-end_date = datetime.now().strftime('%Y-%m-%d')
+for ticker in tickers:
+    
+    tickerData = yf.Ticker(ticker)
 
-# get all stock info
-tickerData = tickerData.history(start='2024-03-15', end=end_date)
+    end_date = datetime.now().strftime('%Y-%m-%d')
 
-tickerHistory = {}
+    # get all stock info
+    tickerData = tickerData.history(start='2024-03-15', end=end_date)
 
-for date in (tickerData.index):
-    date = str(date.month) + '/' + str(date.day) + '/' + str(date.year)
-    tickerHistory[date] = {}
-    for cat in tickerData.iloc[0,:].index:
-        info = tickerData.iloc[0,:][cat]
-        tickerHistory[date][cat] = info
+    tickerHistory = {}
 
-data = {
-    ticker: tickerHistory
-}
+    for date in (tickerData.index):
+        date = str(date.month) + '/' + str(date.day) + '/' + str(date.year)
+        tickerHistory[date] = {}
+        for cat in tickerData.iloc[0,:].index:
+            info = tickerData.iloc[0,:][cat]
+            tickerHistory[date][cat] = info
+
+    data[ticker] = tickerHistory
 
 print(data)
 
