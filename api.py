f2b7a3bbf1c4f845688e48b6517598c7baf3f1bb
aguilarcarboni <aguilarcarboni@gmail.com>
2024-04-04 11:24:17 -0600

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,8 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 #Main
-tickers = ['SPY', 'QQQ', 'DIA', 'NVDA', 'AAPL']
+#tickers = ['SPY', 'QQQ', 'TSLA', 'NVDA', 'AAPL', 'MSFT']
+tickers = ['AAPL']
 
 data = {}
 
@@ -18,15 +19,22 @@ for ticker in tickers:
     tickerData = tickerData.history(start='2024-03-15', end=end_date)
 
     tickerHistory = {}
+    prevDate = '3/15/2024'
 
     for date in (tickerData.index):
         date = str(date.month) + '/' + str(date.day) + '/' + str(date.year)
         tickerHistory[date] = {}
         for cat in tickerData.iloc[0,:].index:
-            info = tickerData.iloc[0,:][cat]
+            info = tickerData.loc[date,:][cat]
             tickerHistory[date][cat] = info
 
     data[ticker] = tickerHistory
+
+for ticker in data:
+    for date in data[ticker]:
+        data[ticker][date]['Change $'] = data[ticker][date]['Close'] - data[ticker][prevDate]['Close']
+        data[ticker][date]['Change %'] = (data[ticker][date]['Close'] - data[ticker][prevDate]['Close'])/data[ticker][date]['Close'] * 100
+        prevDate = date
 
 print(data)
 
