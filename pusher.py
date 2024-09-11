7cd8244f308133de88a20c81469a747e4881e35d
Andres Aguilar <aguilarcarboni@gmail.com>
2024-09-10 19:03:29 -0600

    Eyyy

--- pusher.py
+++
@@ -1,8 +0,0 @@
-import requests as rq
-
-api_url = 'http://127.0.0.1:5001'
-endpoint = '/api/pusher'
-payload = {'message': 'Hello, World!'}
-
-response = rq.post(api_url + endpoint, json=payload)
-print(response.json())
