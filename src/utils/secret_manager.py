37e9e81c2a0e22327bdd3cbfe97662360efd6cb4
Andres Aguilar <aguilarcarboni@gmail.com>
2025-03-09 18:32:15 -0600

    Update secret manager

--- src/utils/secret_manager.py
+++ src/utils/secret_manager.py
@@ -4,7 +4,6 @@ import time
 from laserfocus.utils.exception import handle_exception
 from laserfocus.utils.logger import logger
 
-@handle_exception
 def get_secret(secret_id):
     try:
         logger.info(f"Fetching secret: {secret_id}")
